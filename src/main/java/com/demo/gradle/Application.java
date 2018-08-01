802c2df1981468b0054983a566489a874bd30467
li-qiang <kata.qlee@gmail.com>
2018-08-01 11:56:22 +0800

    Init

---
+++ src/main/java/com/demo/gradle/Application.java
@@ -0,0 +1,8 @@
+package com.demo.gradle;
+
+public class Application {
+    public static void main(String[] args) {
+        String name = args[0];
+        System.out.print("Hello world, " + name);
+    }
+}
